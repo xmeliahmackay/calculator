ea021a91e28abc76e583f50155bf1e136fa72364
Meliah MacKay <xmeilahmackay@gmail.com>
2020-02-08 21:37:00 -0800

    make a form that performs mathmatical equations

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+// Business (or back-end) logic:
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// Everything below this line is user interface (or front-end) logic:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+    event.preventDefault();
+  });
+
+  $("form#subtract").submit(function(event) {
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+    var result = subtract(number1, number2);
+    $("#output2").text(result);
+    event.preventDefault();
+  });
+  $("form#multiply").submit(function() {
+    var number1 = parseInt($("#mult1").val());
+    var number2 = parseInt($("#mult2").val());
+    var result = multiply(number2, number1);
+    $("#output3").text(result);
+    event.preventDefault();
+
+  });
+
+
+});
